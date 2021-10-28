ed8b8a76436026b88cec81369d9fa462fdc205ed
zhangyu <>
2021-10-28 09:27:50 +0800

    first commit
    构建项目

---
+++ app/src/main/java/com/zy/kotlin_demo/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.zy.kotlin_demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * 参照第一行代码（3版）
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
